3620b550fcdd73e5ad8f17c53f7d07b6d5b5bd65
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2024-04-03 21:34:36 -0300

    Modificando Tarefas

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -9,10 +9,11 @@ type TagProps = {
 function retornaCorDeFundo(props: TagProps): string {
   if ('status' in props) {
     if (props.status === 'pendente') return variaveis.amarelo
-    if (props.status === 'cloncluída') return variaveis.verde
+    if (props.status === 'concluido') return variaveis.verde
+    return `#ccc`
   } else if ('prioridade' in props) {
     if (props.prioridade === 'urgente') return variaveis.vermelho
-    if (props.prioridade === 'imoportante') return variaveis.amarelo2
+    if (props.prioridade === 'importante') return variaveis.amarelo2
   }
   return '#ccc'
 }
@@ -35,41 +36,41 @@ export const Tag = styled.span<TagProps>`
   font-weigth: bold;
   color: #fff;
   background-color: ${(props) => retornaCorDeFundo(props)};
-  border - radius: 8px;
-  margin - right: 16px;
-  display: inline - block;
+  border-radius: 8px;
+  margin-right: 16px;
+  display: inline-block;
 `
 export const Descricao = styled.textarea`
-color: #8b8b8b;
-font - size: 14px;
-line - height: 24px;
-font - family: 'Roboto Mono', monospace;
-display: block;
-width: 100 %;
-margin - bottom: 16px;
-margin - top: 16px;
-resize: none;
-border: none;
-background - color: transparent;
+  color: #8b8b8b;
+  font-size: 14px;
+  line-height: 24px;
+  font-family: 'Roboto Mono', monospace;
+  display: block;
+  width: 100%;
+  margin-bottom: 16px;
+  margin-top: 16px;
+  resize: none;
+  border: none;
+  background-color: transparent;
 `
 export const BarraAcoes = styled.div`
-border - top: 1px solid rgba(0, 0, 0, 0.1);
-padding - top: 16px;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  padding-top: 16px;
 `
 export const Botao = styled.button`
-font - weight: bold;
-font - size: 12px;
-color: #fff;
-padding: 8px 12px;
-border: none;
-cursor: pointer;
-background - color: #2f3640;
-border - radius: 8px;
-margin - right: 8px;
+  font-weight: bold;
+  font-size: 12px;
+  color: #fff;
+  padding: 8px 12px;
+  border: none;
+  cursor: pointer;
+  background-color: #2f3640;
+  border-radius: 8px;
+  margin-right: 8px;
 `
 export const BotaoSalvar = styled(Botao)`
-background - color: ${variaveis.verde};
+  background-color: ${variaveis.verde};
 `
 export const BotaoCancelarRemover = styled(Botao)`
-background - color: ${variaveis.vermelho};
+  background-color: ${variaveis.vermelho};
 `
